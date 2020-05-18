2d73863c915d6b7ac8e1e6eb68098af66b42e9f5
roi <roipeker@gmail.com>
2020-05-18 18:12:29 -0300

    removed an useless, left over, src folder

--- move_back/credentials.h
+++
@@ -1,4 +0,0 @@
-#define FIREBASE_HOST "https://vkot-doorbell.firebaseio.com"
-#define FIREBASE_AUTH "vhGpAVldOEy11xoBZr2wkZHa3zlMMCx2jAYjPBdO"
-#define LED_INVERTED false
-#define LED_INVERTED false
